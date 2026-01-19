dc97672639ef92dbae0d8951a6c610294bd99326
Keito Tadano <0504keito@gmail.com>
2026-01-19 10:51:29 +0900

    cange test

--- test/test.bash
+++ test/test.bash
@@ -7,7 +7,7 @@ ng () {
     exit 1
 }
 
-cd /root/ros2_ws || ng
+cd ~/ros2_ws || ng
 colcon build > /dev/null 2>&1 || ng
 
 source /opt/ros/humble/setup.bash
@@ -16,8 +16,9 @@ source install/setup.bash
 timeout 10 ros2 launch mypkg talk_listen.launch.py \
   > /tmp/mypkg.log 2>&1 || true
 
-grep -q "Load Talker Node started" /tmp/mypkg.log || ng
-grep -q "Listener" /tmp/mypkg.log || ng
+grep -q "Test Talker Node started" /tmp/mypkg.log || ng
+grep -q "Threshold Listener Node started" /tmp/mypkg.log || ng
+grep -E "LOW|MID|HIGH" /tmp/mypkg.log || ng
 
 echo "OK"
 exit 0
